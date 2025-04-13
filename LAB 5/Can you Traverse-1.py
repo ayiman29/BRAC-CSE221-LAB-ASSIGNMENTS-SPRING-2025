e29f9b08f1bda144f6a1b731e2b6b1b715a78fc4
RZ Ayiman <ayimaneg@gmail.com>
2025-04-13 13:07:45 +0600

    Updated LAB 5

---
+++ LAB 5/Can you Traverse-1.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+n, m = map(int, input().split())
+graph = defaultdict(list)
+for _ in range(m):
+    u, v= map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+'''
+'''
